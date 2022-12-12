b9d1fe17ae68d315f635c0f49170e8974d80b6df
Tommaso Frecentese <88935008+aske666@users.noreply.github.com>
2022-12-12 08:41:45 +0100

    Update index.php

--- wiki/index.php
+++ wiki/index.php
@@ -1,1 +1,11 @@
-index.php home page - test
+<!DOCTYPE html>
+<html lang='en' xmlang='en'>
+  <head>
+    <meta charset='UTF-8'>
+    <title>Homepage - php test</title>
+  </head>
+  
+  <body>
+    <p>index.php - test</p>
+  </body>
+</html>
